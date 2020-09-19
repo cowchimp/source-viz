40e0f5f9c271acabfae7aef39f75425d57cf7c72
cowchimp <cowchimp@users.noreply.github.com>
2020-09-19 14:00:45 +0300

    add vscode extension package

---
+++ packages/astscout-vscode/src/extension.ts
@@ -0,0 +1,52 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "hello-vscode-extension" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerTextEditorCommand('astscout-vscode.helloWorld', (textEditor) => {
+    // The code you place here will be executed every time your command is executed
+
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World from Hello VSCode Extension!!!');
+
+    const panel = vscode.window.createWebviewPanel(
+      'catCoding', // Identifies the type of the webview. Used internally
+      'Cat Coding', // Title of the panel displayed to the user
+      vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
+      {} // Webview options. More on these later.
+    );
+
+    const content = textEditor.document.getText();
+    panel.webview.html = getWebviewContent(content);
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+function getWebviewContent(str: string) {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+    <code>${str}</code>
+</body>
+</html>`;
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
